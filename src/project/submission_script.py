54c6a413485ad5cf0260fd060567346d5b767063
Evan Vogelbaum <evanv@mit.edu>
2022-03-23 22:20:43 -0400

    Modified submission script to include classifiers

--- src/project/submission_script.py
+++ src/project/submission_script.py
@@ -3,14 +3,45 @@ import sys
 import numpy as np
 import pandas as pd
 from os.path import join
+from project.classifiers import MaleEvalClassifier, FemaleEvalClassifier
+from tqdm import tqdm
 hello_world()
 
-if __name__ == '__main__':
+def load_test_data() -> pd.DataFrame:
+    """
+    Returns a pandas DataFrame with all data relevant for evaluation. Each row has at minimum:
+    - `id`: the id of the test pn_history x feature x case pair
+    - `case_num`: the case number for the test sample
+    - `pn_num`: the pn identifier for the test sample
+    - `feature_text`: the text of the feature for the test sample
+    - `feature_num`: the identifier of the feature for the test sample
+    - `pn_history`: the text for the patient note of this test sample
+    # TODO: avoid tokenizing patient notes more than once when we get to that point
+    """
     DATA_PATH = 'data/' if len(sys.argv) == 1 else sys.argv[1]
     test = pd.read_csv(join(DATA_PATH, 'test.csv'))
-    dummy_predictions = []
-    for test_id in test['id']:
-        dummy_predictions.append(f'{np.random.randint(100)} {np.random.randint(101, 200)}')
-    test['location'] = dummy_predictions
+    features = pd.read_csv(join(DATA_PATH, 'features.csv'))
+    patient_notes = pd.read_csv(join(DATA_PATH, 'patient_notes.csv'))
+    test = test.merge(features, on='feature_num').merge(patient_notes, on='pn_num')
+    return test
+
+if __name__ == '__main__':
+    # Loading data
+    test = load_test_data()
+    # Making predictions
+    predictions = []
+    male_classifier, female_classifier = MaleEvalClassifier(), FemaleEvalClassifier()
+    for idx, r in tqdm(test.iterrows(), desc='Eval:', total=len(test)):
+        if r['feature_text'] == 'Male':
+            prediction = male_classifier.predict(r["pn_history"])
+            bounds = f'{0} {len(r["pn_history"])}' if prediction == 1 else ''
+            predictions.append(bounds)
+        elif r['feature_text'] == 'Female':
+            prediction = female_classifier.predict(r["pn_history"])
+            bounds = f'{0} {len(r["pn_history"])}' if prediction == 1 else ''
+            predictions.append(bounds)
+        else:
+            predictions.append(f'{np.random.randint(100)} {np.random.randint(101, 200)}')
+    test['location'] = predictions
     submission = test[['id', 'location']]
     submission.to_csv('submission.csv', index=False)
