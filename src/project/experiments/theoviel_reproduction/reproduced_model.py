955ab25b60b3a4207d899aec02cf6ed77029fffb
Evan Vogelbaum <evanv@mit.edu>
2022-04-09 16:58:40 -0400

    Fully reproduced dataset

--- src/project/experiments/theoviel_reproduction/reproduced_model.py
+++ src/project/experiments/theoviel_reproduction/reproduced_model.py
@@ -1,7 +1,10 @@
+import pandas as pd
 from transformers import BatchEncoding, AutoTokenizer
 import torch
-
-
+from typing import List, Tuple
+import numpy as np
+from torch.utils.data import Dataset
+from project.data.data_loaders import get_clean_train_data
 # Copied verbatim from notebook. TODO: refactor out
 class CFG:
     wandb=False
@@ -24,7 +27,7 @@ class CFG:
     betas=(0.9, 0.999)
     batch_size=12
     fc_dropout=0.2
-    max_len=512
+    max_len=466  # NOTE: This gets changed in the middle of notebook from 512 to 466
     weight_decay=0.01
     gradient_accumulation_steps=1
     max_grad_norm=1000
@@ -34,8 +37,6 @@ class CFG:
     train=True
 tokenizer = AutoTokenizer.from_pretrained(CFG.model)
 tokenizer.save_pretrained('tokenizer/')
-CFG.tokenizer = tokenizer
-
 
 def encode_input(pn_history: str, feature_text: str) -> BatchEncoding:
     """
@@ -45,26 +46,66 @@ def encode_input(pn_history: str, feature_text: str) -> BatchEncoding:
         where 1 indicates that feature `feature_text` is present in the token.
     :param pn_history: The patient note
     :param feature_text: The feature text
-    :return: The Batch Encoding of the pair of inputs with the following fi
+    :return: The Batch Encoding of the pair of inputs with the following format:
+        .input_ids: torch.tensor (CFG.max_len), sequence of tokens the input is mapped to, given by the id (int)
+        .token_type_ids: torch.tensor (CFG.max_len), sequence of ids indicating what sentence a token belongs to (int)
+        .attention_mask: torch.tensor (CFG.max_len), sequence of 1s and 0s indicating which tokens should be attended to (int)
     """
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-base")
-    inputs = tokenizer(pn_history, feature_text, add_special_tokens=True,
-     max_length=512, padding='max_length', return_offsets_mapping=False)
+    inputs:BatchEncoding = tokenizer(pn_history, feature_text, add_special_tokens=True, max_length=CFG.max_len, padding='max_length', return_offsets_mapping=False)
     for key, value in inputs.items():
         inputs[key] = torch.tensor(value, dtype=torch.long)
     return inputs
-    
+
+def create_label(text: str, location_list: List[Tuple[int, int]]) -> torch.Tensor:
+    """
+    Creates a label for the tokens correpsonding to input_text given a list of start and end locations (in the input text) of characters that should be given label 1
+    Label gives -1 to special tokens, 0 to tokens with no characters they correspond to in location_list, and 1 to all other tokens
+    Ex. 'Hello, how are you', [(0, 4), (15,17)] tokenized as ['[CLS]', 'Hello','[SEP]' , 'how','[SEP]', 'are','[SEP]', 'you'] receives label [-1, 1, -1, 0, -1, 0 -1, 1]
+    :param text: The input text
+    :param location_list: The list of start and end locations of characters that should be given label 1
+    :return: The label for the tokens corresponding to input_text 
+    """
+    encoded:BatchEncoding = tokenizer(text, add_special_tokens=True, max_length=CFG.max_len, padding="max_length", return_offsets_mapping=True)
+    offset_mapping:List[Tuple[int, int]] = encoded['offset_mapping'] # Maps back to the original text (e.g. 'Hello' -> 'He', 'llo', [(0, 2), (2, 5)])
+    ignore_idxes:np.ndarray = np.where(np.array(encoded.sequence_ids()) != 0)[0]  # Indexes of the special tokens, see https://huggingface.co/docs/transformers/v4.17.0/en/main_classes/tokenizer#transformers.BatchEncoding
+    label = np.zeros(len(offset_mapping)) # Create a label for each token
+    label[ignore_idxes] = -1 # Special tokens are given label -1 by default
+    if not location_list: return torch.tensor(label, dtype=torch.float)  # If no locations are given, return the label
+    for label_start, label_end in location_list:
+        start_idx:int = None
+        end_idx:int = None
+        for idx, (token_start, token_end) in enumerate(offset_mapping):
+            if (start_idx is None) and (label_start < token_start):
+                start_idx = idx - 1 # Give start the first token before the first token contained in [start, end]
+            if (end_idx is None) and (label_end <= token_end):
+                end_idx = idx + 1 # Give end the first token after the last token contained in [start, end]
+        if start_idx is None:
+            start_idx = end_idx
+        if (start_idx != -1) and (end_idx != -1):
+            label[start_idx:end_idx] = 1 # Give a token label of 1 if the idxs it corresponds to are within (start, end)
+    return torch.tensor(label, dtype=torch.float)
 
 
-def prepare_input(cfg, text, feature_text):
+class TrainDataset(Dataset):
     """
-    Copied verbatim from notebook. TODO: refactor out
+    Represents and immutable training dataset.
     """
-    inputs = cfg.tokenizer(text, feature_text, 
-                           add_special_tokens=True,
-                           max_length=CFG.max_len,
-                           padding="max_length",
-                           return_offsets_mapping=False)
-    for k, v in inputs.items():
-        inputs[k] = torch.tensor(v, dtype=torch.long)
-    return inputs
+    def __init__(self, df: pd.DataFrame):
+        self._TRAIN = df.copy(deep=True)
+
+    def __len__(self):
+        return len(self._TRAIN)
+
+    def __getitem__(self, idx: int) -> Tuple[BatchEncoding, torch.Tensor]:
+        row:pd.Series = self._TRAIN.iloc[idx]
+        inputs = encode_input(row['pn_history'], row['feature_text'])
+        label = create_label(row['pn_history'], row['location'])
+        return inputs, label
+
+"""
+Next steps: 
+- Add in model class, ensure it is compatible with Train Dataset. Note that it takes a *significant* amount of RAM just to run the model once. 
+- Addi in train loop and get this thing training!
+
+
+"""
--- src/project/experiments/theoviel_reproduction/test_reproduced_model.py
+++ src/project/experiments/theoviel_reproduction/test_reproduced_model.py
@@ -1,6 +1,9 @@
-from project.experiments.theoviel_reproduction.reproduced_model import encode_input
+from venv import create
+from project.experiments.theoviel_reproduction.reproduced_model import encode_input, create_label, TrainDataset
 import numpy as np
 from project.data.data_loaders import get_clean_train_data
+from typing import Final
+TRAIN:Final = get_clean_train_data()
 
 def test_empty_input():
     encoding = encode_input("", "")
@@ -14,9 +17,8 @@ def test_empty_input():
     assert np.all(attention_mask[2:] == 0)
 
 def test_patient_note_and_feature():
-    train = get_clean_train_data()
-    HISTORY = train.pn_history.values[0]
-    FEATURE = train.feature_text.values[0]
+    HISTORY = TRAIN.pn_history.values[0]
+    FEATURE = TRAIN.feature_text.values[0]
     encoding = encode_input(HISTORY, FEATURE)
     input_ids:np.ndarray = encoding.input_ids.numpy()
     token_type_ids:np.ndarray = encoding.token_type_ids.numpy()
@@ -27,3 +29,24 @@ def test_patient_note_and_feature():
     assert np.all(token_type_ids == 0)
     assert np.all(attention_mask[0:2] == [1,1])
     assert len(np.nonzero(attention_mask)[0]) == 270
+
+def test_create_label_single_loc():
+    label:np.ndarray = create_label(TRAIN.loc[20, 'pn_history'], TRAIN.loc[20, 'location']).numpy()
+    assert label.shape == (466,)
+    assert label[0] == -1
+    assert np.all(np.where(label == 1)[0] == [101, 102])
+
+
+def test_create_label_multi_loc():
+    label:np.ndarray = create_label(TRAIN.loc[3, 'pn_history'], TRAIN.loc[3, 'location']).numpy()
+    assert label.shape == (466,)
+    assert label[0] == -1
+    assert np.all(np.where(label == 1)[0] == [20, 21, 43])
+
+def test_train_dataset():
+    data = TrainDataset(TRAIN)
+    assert len(data) == len(TRAIN)
+    (test_X, test_y) = data[42]
+    assert test_X.input_ids[0].item() == 1
+    assert test_y[39].item() == 1
+
