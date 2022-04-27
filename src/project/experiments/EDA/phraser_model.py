bb87f6573af25d1aeb8d5dbcb0a4f6afcde8c5fd
yuvalmamana <yuvalm11@mit.edu>
2022-04-27 00:22:38 -0400

    phrases model in ready for submission script

--- src/project/experiments/EDA/phraser_model.py
+++ src/project/experiments/EDA/phraser_model.py
@@ -36,16 +36,17 @@ def process_feature_text(text: str) -> str:
     return text
 
 
-def get_features():
+def get_features(case=None):
     features = data_loaders.get_features()
     features['feature_text'] = features['feature_text'].apply(process_feature_text)
-
+    if case is not None:
+        features = features[features['case_num'] == case].reset_index(drop=True)
     return features
 
 
-def train_bert(sentences: list):
+def train_bert(sentences: list, show_pbar=False):
     model = SentenceTransformer('all-mpnet-base-v2')
-    embeddings = model.encode(sentences, show_progress_bar=True)
+    embeddings = model.encode(sentences, show_progress_bar=show_pbar)
 
     return embeddings, model
 
@@ -61,23 +62,25 @@ def bert_similarity(s: str, embeddings, model: SentenceTransformer) -> (int, flo
     return opt_index, sim[opt_index][0]
 
 
-def predict(feature_num: int, pn_note: str, embeddings, model, threshold=0.47) -> list:
+def predict(feature_num: int, case_num:int, pn_note: str, threshold=0.47) -> list:
     """
     returns a list of tuples of predicted locations of a given feature in a given text
     """
+    features = get_features(case_num)
+    embeddings, model = train_bert(features['feature_text'])
     phrases = phrase(pn_note)
     features = get_features()
     start = 0
     locations = []
     similarities = []
     for phrase_txt in phrases:
-        end = start + len(phrase)
-        index, sim = bert_similarity(phrase, embeddings, model)
+        end = start + len(phrase_txt)
+        index, sim = bert_similarity(phrase_txt, embeddings, model)
         f_num = features['feature_num'][index]
         if f_num == feature_num and sim > threshold:
             similarities.append(sim)
             locations.append((start, end))
-        start += len(phrase) + 2
+        start += len(phrase_txt) + 2
 
     return locations
 
@@ -94,8 +97,8 @@ def prepare_test_data() -> pd.DataFrame:
     return test
 
 
-
 test = prepare_test_data()
-features = get_features()
-embeddings, model = train_bert(features['feature_text'])
-test['pred'] = test.apply(lambda row: predict(row['feature_num'], row['pn_history'], embeddings, model), axis=1)
+test['pred'] = test.apply(lambda row: predict(row['feature_num'],
+                                              row['case_num'],
+                                              row['pn_history'],
+                                              threshold=0.37), axis=1)
